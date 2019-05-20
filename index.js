01148072fdc3021bb080a82ec07e8be2600f1a13
asd0999 <obelixotic@gmail.com>
2019-05-20 19:31:30 -0400

    3 databases

--- index.js
+++ index.js
@@ -17,44 +17,48 @@ client = new Client({
 
 client.connect();
 
-var inputFile = './web/assets/aptest-01.csv';
+var inputFile_ap1 = './web/assets/aptest-01.csv'; //make this aptest1-01.csv
+var inputFile_ap2 = './web/assets/aptest-01.csv'; //make this aptest2-01.csv
+var inputFile_ap3 = './web/assets/aptest-01.csv'; //make this aptest3-01.csv
 
 const PORT = process.env.PORT || 8000;
 
 var fs = require('fs');
 var parse = require('csv-parse');
 
-setInterval(csv2pg, 10000);
+setInterval(csv2pg_ap1, 10000);
+setInterval(csv2pg_ap2, 10000);
+setInterval(csv2pg_ap3, 10000);
 
-function csv2pg() {
-  var csvNew = [];
-  var counter = 0;
+function csv2pg_ap1() {
+  var csvNew_ap1 = [];
+  var counter_ap1 = 0;
 
-  fs.createReadStream('./web/assets/aptest-01.csv')
+  fs.createReadStream(inputFile_ap1)
     .pipe(parse({
       delimiter: ';'
     }))
-    .on('data', function(oldcsvrow) {
-      console.log(oldcsvrow); //print each row of csv file
+    .on('data', function(oldcsvrow_ap1) {
+      console.log(oldcsvrow_ap1); //print each row of csv file
       // counter = 0;
-      if (counter > 4) {
-        csvNew.push(oldcsvrow);
+      if (counter_ap1 > 4) {
+        csvNew_ap1.push(oldcsvrow_ap1);
       }
-      counter++;
+      counter_ap1++;
       client.query('DELETE FROM devices', function(e, r) {
-        console.log('deleting old entries');
+        // console.log('deleting old entries');
       });
 
     })
     .on('end', function() {
-      console.log('new csv');
-      console.log(csvNew); //print each row of new csv file excluding the first 4 rows
+      console.log('new csv AP 1');
+      console.log(csvNew_ap1); //print each row of new csv file excluding the first 4 rows
 
-      csvNew.forEach(element => {
+      csvNew_ap1.forEach(element => {
         let arr = element[0].split(',');
         // console.log(arr[0]);
         client.query('INSERT INTO devices (station, ap) VALUES ($1, $2)', [arr[0], arr[5]], function(error, results) {
-          console.log('adding new entries');
+          // console.log('adding new entries');
         });
       });
       // client.query('DELETE FROM devices ORDER BY id DESC LIMIT 1', function(error, results) {
@@ -67,7 +71,109 @@ function csv2pg() {
     // console.log("got data request from main.js");
     client.query('SELECT * FROM devices', function(err, result) {
       if (err) {
-        console.log(error);
+        console.log(err);
+      }
+      for (let row of result.rows) {
+        message.push(row);
+      }
+      res1.send(message);
+    });
+  });
+}
+
+function csv2pg_ap2() {
+  var csvNew_ap2 = [];
+  var counter_ap2 = 0;
+
+  fs.createReadStream(inputFile_ap2)
+    .pipe(parse({
+      delimiter: ';'
+    }))
+    .on('data', function(oldcsvrow_ap2) {
+      console.log(oldcsvrow_ap2); //print each row of csv file
+      // counter = 0;
+      if (counter_ap2 > 4) {
+        csvNew_ap2.push(oldcsvrow_ap2);
+      }
+      counter_ap2++;
+      client.query('DELETE FROM devices2', function(e, r) {
+        // console.log('deleting old entries2');
+      });
+
+    })
+    .on('end', function() {
+      console.log('new csv AP 2');
+      console.log(csvNew_ap2); //print each row of new csv file excluding the first 4 rows
+
+      csvNew_ap2.forEach(element => {
+        let arr = element[0].split(',');
+        // console.log(arr[0]);
+        client.query('INSERT INTO devices2 (station, ap) VALUES ($1, $2)', [arr[0], arr[5]], function(error, results) {
+          // console.log('adding new entries2');
+        });
+      });
+      // client.query('DELETE FROM devices ORDER BY id DESC LIMIT 1', function(error, results) {
+      //   console.log('deleted last blank entry');
+      // });
+    });
+
+  app.get('/getData2', function(req, res1) {
+    let message = [];
+    // console.log("got data request from main.js");
+    client.query('SELECT * FROM devices2', function(err, result) {
+      if (err) {
+        console.log(err);
+      }
+      for (let row of result.rows) {
+        message.push(row);
+      }
+      res1.send(message);
+    });
+  });
+}
+
+function csv2pg_ap3() {
+  var csvNew_ap3 = [];
+  var counter_ap3 = 0;
+
+  fs.createReadStream(inputFile_ap3)
+    .pipe(parse({
+      delimiter: ';'
+    }))
+    .on('data', function(oldcsvrow_ap3) {
+      console.log(oldcsvrow_ap3); //print each row of csv file
+      // counter = 0;
+      if (counter_ap3 > 4) {
+        csvNew_ap3.push(oldcsvrow_ap3);
+      }
+      counter_ap3++;
+      client.query('DELETE FROM devices3', function(e, r) {
+        // console.log('deleting old entries3');
+      });
+
+    })
+    .on('end', function() {
+      console.log('new csv AP 3');
+      console.log(csvNew_ap3); //print each row of new csv file excluding the first 4 rows
+
+      csvNew_ap3.forEach(element => {
+        let arr = element[0].split(',');
+        // console.log(arr[0]);
+        client.query('INSERT INTO devices3 (station, ap) VALUES ($1, $2)', [arr[0], arr[5]], function(error, results) {
+          // console.log('adding new entries3');
+        });
+      });
+      // client.query('DELETE FROM devices ORDER BY id DESC LIMIT 1', function(error, results) {
+      //   console.log('deleted last blank entry');
+      // });
+    });
+
+  app.get('/getData3', function(req, res1) {
+    let message = [];
+    // console.log("got data request from main.js");
+    client.query('SELECT * FROM devices3', function(err, result) {
+      if (err) {
+        console.log(err);
       }
       for (let row of result.rows) {
         message.push(row);
--- web/js/main_walker.js
+++ web/js/main_walker.js
@@ -1,7 +1,13 @@
 let floorMap;
-let clients = [];
+let clients_ap1 = [];
+let clients_ap2 = [];
+let clients_ap3 = [];
 let x1 = 600;
 let y1 = 100;
+let x2 = 1000;
+let y2 = 130;
+let x3 = 400;
+let y3 = 100;
 let range = 350;
 
 function preload() {
@@ -24,40 +30,82 @@ function setup() {
   //        text(clients[i], random(660-150,660+150), random(0,100+150));
   //   }
 
-  getData('/getData1'); //access point 1
-  // getData('/getData2'); //access point 2
-  // getData('/getData3'); //access point 3
+  getData1('/getData1'); //access point 1
+  getData2('/getData2'); //access point 2
+  getData3('/getData3'); //access point 3
   // getData('/getData4'); //access point 4
 }
 
-setTimeout(timer, 5000);
+setTimeout(timer, 100);
 
 function timer() {
   setInterval(function() {
-    getData('/getData1');
+    getData1('/getData1');
+    getData2('/getData2');
+    getData3('/getData3');
     console.log('update!');
   }, 7000);
 
 }
 
 
-function getData(url) {
+function getData1(url) {
   console.log('got data');
   fetch(url)
     .then((response) => {
       return response.json();
     })
     .then((jsonData) => {
-      clients = [];
+      clients_ap1 = [];
       jsonData.forEach(element => {
         console.log(element);
         // text(element.station, random(660 - 150, 660 + 150), random(0, 100 + 150));
-        clients.push(element.station);
+        clients_ap1.push(element.station);
       });
-      for (var i = 0; i < clients.length; i++) {
-        clients[i] = new Target(clients[i], x1, y1);
+      for (var i = 0; i < clients_ap1.length; i++) {
+        clients_ap1[i] = new Target(clients_ap1[i], x1, y1);
       }
-      // clients.splice(clients.length - 1, 1);
+      // clients_ap1.splice(clients.length - 1, 1);
+    });
+}
+
+function getData2(url) {
+  console.log('got data');
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonData) => {
+      clients_ap2 = [];
+      jsonData.forEach(element => {
+        console.log(element);
+        // text(element.station, random(660 - 150, 660 + 150), random(0, 100 + 150));
+        clients_ap2.push(element.station);
+      });
+      for (var i = 0; i < clients_ap2.length; i++) {
+        clients_ap2[i] = new Target(clients_ap2[i], x2, y2);
+      }
+      // clients_ap1.splice(clients.length - 1, 1);
+    });
+}
+
+function getData3(url) {
+  console.log('got data');
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonData) => {
+      clients_ap3 = [];
+      jsonData.forEach(element => {
+        console.log(element);
+        // text(element.station, random(660 - 150, 660 + 150), random(0, 100 + 150));
+        clients_ap3.push(element.station);
+      });
+      for (var i = 0; i < clients_ap3.length; i++) {
+        clients_ap3[i] = new Target(clients_ap3[i], x3, y3);
+      }
+      // clients_ap1.splice(clients.length - 1, 1);
     });
 }
 
@@ -73,11 +121,21 @@ function draw() {
   ellipse(660, 100, 350, 350);
 
   fill(0);
-  for (var i = 0; i < clients.length; i++) {
-    clients[i].display();
-    clients[i].move();
+  for (var i = 0; i < clients_ap1.length; i++) {
+    clients_ap1[i].display();
+    clients_ap1[i].move();
   }
-  // console.log(clients.length);
+  // console.log(clients_ap1.length);
+
+  for (var i = 0; i < clients_ap2.length; i++) {
+    clients_ap2[i].display();
+    clients_ap2[i].move();
+  }
+
+  for (var i = 0; i < clients_ap3.length; i++) {
+    clients_ap3[i].display();
+    clients_ap3[i].move();
+  }
 
 }
 
